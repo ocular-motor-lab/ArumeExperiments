e0b925f5829bd1922d2c807e596363ea56681061
Roksana Sadeghi <roksana.s.sadeghi@gmail.com>
2023-04-07 12:34:20 -0700

    added the openIris eye tracker client dll to the arume path, and hard coded the ip number

--- +ArumeHardware/@VOG/VOG.m
+++ +ArumeHardware/@VOG/VOG.m
@@ -14,53 +14,55 @@ classdef VOG  < handle
             if ( ~exist('port','var') || isempty(port) )
                 port = 9000;
             end
-
-            if ( ~exist('openirispath','var') || isempty(openirispath) )
-                openirispath = 'M:\TEMP\Arume_openiris_test\Debug';
-            end
-            
-            if ( exist(openirispath) )
-                asm = NET.addAssembly(fullfile(openirispath,'EyeTrackerRemoteClient.dll'));
-                if ( ~exist('ip','var') )
-                    ip = fileread(fullfile(openirispath,'IP.txt'));
-                end
-            elseif ( exist('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug') )
-                asm = NET.addAssembly('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug\IP.txt');
-                end
-            elseif ( exist('C:\secure\Code\EyeTracker\bin\x64\Debug','file') )
-                asm = NET.addAssembly('C:\secure\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\secure\Code\EyeTracker\bin\x64\Debug\IP.txt');
-                end
-            elseif exist('C:\secure\EyeTracker Debug 2018-22-08\EyeTrackerRemoteClient.dll')
-                asm = NET.addAssembly('C:\secure\EyeTracker Debug 2018-22-08\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\secure\EyeTracker Debug 2018-22-08\IP.txt');
-                end
-            elseif exist('C:\Secure\EyeTracker\Debug\EyeTrackerRemoteClient.dll')
-                asm = NET.addAssembly('C:\Secure\EyeTracker\Debug\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\Secure\EyeTracker\Debug\IP.txt');
-                end
-            elseif exist('C:\secure\OpenIris 1.3.7752.29719\Debug\EyeTrackerRemoteClient.dll')
-                asm = NET.addAssembly('C:\secure\OpenIris 1.3.7752.29719\Debug\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\secure\OpenIris 1.3.7752.29719\Debug\IP.txt');
-                end
-            elseif exist('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll')
-                asm = NET.addAssembly('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll');
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\IP.txt');
-                end
-            else
-                asm = NET.addAssembly('C:\secure\code\Debug\EyeTrackerRemoteClient.dll');
-                
-                if ( ~exist('ip','var') )
-                    ip = fileread('C:\secure\code\Debug\IP.txt');
-                end
-            end
+            arumeUtilPath = regexpi(path,['[^;]*arumeutil'],'match');
+            asm = NET.addAssembly(fullfile(arumeUtilPath{1},'EyeTrackerRemoteClient.dll'));
+            ip = '127.0.0.1';
+%             if ( ~exist('openirispath','var') || isempty(openirispath) )
+%                 openirispath = 'M:\TEMP\Arume_openiris_test\Debug';
+%             end
+% 
+%             if ( exist(openirispath) )
+%                 asm = NET.addAssembly(fullfile(openirispath,'EyeTrackerRemoteClient.dll'));
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread(fullfile(openirispath,'IP.txt'));
+%                 end
+%             elseif ( exist('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug') )
+%                 asm = NET.addAssembly('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('D:\OneDrive\UC Berkeley\OMlab - JOM\Code\OpenIris\source\bin\x64\Debug\IP.txt');
+%                 end
+%             elseif ( exist('C:\secure\Code\EyeTracker\bin\x64\Debug','file') )
+%                 asm = NET.addAssembly('C:\secure\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\secure\Code\EyeTracker\bin\x64\Debug\IP.txt');
+%                 end
+%             elseif exist('C:\secure\EyeTracker Debug 2018-22-08\EyeTrackerRemoteClient.dll')
+%                 asm = NET.addAssembly('C:\secure\EyeTracker Debug 2018-22-08\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\secure\EyeTracker Debug 2018-22-08\IP.txt');
+%                 end
+%             elseif exist('C:\Secure\EyeTracker\Debug\EyeTrackerRemoteClient.dll')
+%                 asm = NET.addAssembly('C:\Secure\EyeTracker\Debug\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\Secure\EyeTracker\Debug\IP.txt');
+%                 end
+%             elseif exist('C:\secure\OpenIris 1.3.7752.29719\Debug\EyeTrackerRemoteClient.dll')
+%                 asm = NET.addAssembly('C:\secure\OpenIris 1.3.7752.29719\Debug\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\secure\OpenIris 1.3.7752.29719\Debug\IP.txt');
+%                 end
+%             elseif exist('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll')
+%                 asm = NET.addAssembly('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\EyeTrackerRemoteClient.dll');
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\Users\jorge\UC Berkeley\OMlab - JOM\Code\EyeTracker\bin\x64\Debug\IP.txt');
+%                 end
+%             else
+%                 asm = NET.addAssembly('C:\secure\code\Debug\EyeTrackerRemoteClient.dll');
+% 
+%                 if ( ~exist('ip','var') )
+%                     ip = fileread('C:\secure\code\Debug\IP.txt');
+%                 end
+%             end
             
 %             this.eyeTracker = VORLab.VOG.Remote.EyeTrackerClient(ip, port);
             this.eyeTracker = OpenIris.EyeTrackerClient(ip, port);

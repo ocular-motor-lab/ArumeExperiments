22431d538bd56c463b5339f79cf332e76ffa0f75
Jorge Otero-Millan <jom@berkeley.edu>
2024-02-17 08:50:09 -0800

    Removed the basic experiments that are in arume

--- +ArumeExperimentDesigns/@OpticFlow_DualTask/RetrieveEyelinkFileManually.m
+++
@@ -1,8 +0,0 @@
-clear; clc; close all;
-
-% make sure eyelink is in tracking mode (not file manager mode)
-srcfilename = 'ArumeTmp.edf';
-destfilename = 'Backup';
-Eyelink('Initialize')
-status=Eyelink('ReceiveFile','ArumeTmp.edf',['C:\Users\pvt-maadm\Desktop\','Backup.edf']);
-
